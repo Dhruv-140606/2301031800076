34221de4bc92c0cc8d99fc547aca26e378a6442a
Dhruv-140606 <2301031800076@silveroakuni.ac.in>
2025-06-27 23:26:02 +0530

    Add files via upload

---
+++ React_Task_1/sou/src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const myFunc = ()=> {
+  return(
+    <>
+      <h1 style={
+        {
+          color:"red"
+        }
+      }>
+        {2+2}
+      </h1>
+    </>
+  )
+}
+export default myFunc;
